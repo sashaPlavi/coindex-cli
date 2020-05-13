8a2034cf3b152d2fe95c4f15b1671550e31cecfa
SashaPlavi <sasha.bingulac@gmail.com>
2020-05-13 10:23:50 +0200

    first fetch

--- bin/coindex-check.js
+++ bin/coindex-check.js
@@ -4,8 +4,10 @@ const { check } = require("../commands/check");
 program
   .command("price")
   .description("check price of coins")
-  .action(() => {
-    check.price();
+  .option("--coin <type>", "Add specific type in CSV format", "BTC, ETH, XRP")
+  .option("--cur <currency>", "Change the curretcy", "EUR")
+  .action((cmd) => {
+    check.price(cmd);
   });
 
 program.parse(process.argv);
--- commands/check.js
+++ commands/check.js
@@ -1,6 +1,20 @@
+const KeyManager = require("../lib/Key-manager");
+const CryptoApi = require("../lib/CryptoApi");
+
 const check = {
-  price() {
-    console.log("bla pricer");
+  async price(cmd) {
+    try {
+      const keyManager = new KeyManager();
+      const key = keyManager.getKey();
+      const api = new CryptoApi(key);
+
+      const priceData = await api.getPriceData(cmd.coin, cmd.cur);
+      console.log(priceData);
+    } catch (error) {
+      console.error(error);
+    }
+
+    //console.log(cmd.coin, cmd.cur);
   },
 };
 
